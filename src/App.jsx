f3c1e4447fd7b64f24808a91c35185ab25f60d57
pradeep-kalyan <pradeepkalyan1275@gmail.com>
2025-01-07 20:54:33 +0530

    ic

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import BalanceCard from "./components/BalanceCard";
 import TransactionForm from "./components/TransactionForm";
 import TransList from "./components/Translist";
+import "./index.css";
 
 function App() {
   const [expense, addexpense] = useState(0);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Fintrack/", // Path for GitHub Pages
+  build: {
+    outDir: "dist", // Output folder for the production build
+  },
+});
